eeb41f9f71deb8cf0d15657134b1b4cadf9f4d0b
Rehwagen <48112666+Rehwagen@users.noreply.github.com>
2019-06-13 23:08:11 -0300

    Add files via upload

---
+++ word generator.cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int fatorial(int n){
+	int r = 1;
+	for(int i = 1; i <= n; i++){
+		r *= i;
+	}
+	return r;
+}
+
+int main(){
+	string s;
+	unsigned int i, t;
+	cout << "Digite as letras (no numero de vezes de cada letra) que voce quer que sejam emaranhadas (sem espacos): " << endl;
+	cin >> s;
+	ofstream lista;
+	lista.open("nomes.txt");
+	if(!lista.is_open()){
+		cout << "Problema na criação/manipulacao do arquivo!" << endl;
+		return 1;
+	}
+	t = fatorial(s.size());
+	cout << "Numero de palavras: " << t << endl;
+	i = 1;
+	sort(s.begin(), s.end());
+	lista << s << '\n';
+	while(next_permutation(s.begin(), s.end())){
+		lista << s << '\n';
+		i++;
+		if(i == t/100){
+			cout << '*';
+			i = 0;
+		}
+	}
+	lista.close();
+	return 0;
+}
